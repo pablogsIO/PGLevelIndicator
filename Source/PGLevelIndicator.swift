fda68a1d46ee7ba5a4d6a1b12a6ae3ada2926401
Pablo Garcia <pablo@pablogs.io>
2018-10-20 21:57:18 +0200

    Added source folder

---
+++ Source/PGLevelIndicator.swift
@@ -0,0 +1,55 @@
+//
+//  PGLevelIndicator.swift
+//  PGLevelIndicator
+//
+//  Created by Pablo on 17/10/2018.
+//  Copyright © 2018 Pablo Garcia. All rights reserved.
+//
+
+import UIKit
+
+struct ItemParameters {
+    var title: String
+    var titleColor: UIColor
+    var level: Level
+}
+
+class PGLevelIndicator: UIView {
+
+    private var levelIndicators = [PGLevelIndicatorView]()
+
+    init(frame: CGRect, itemsParameters: [ItemParameters]) {
+        super.init(frame: frame)
+        let factorIncrement = 0.2
+        var factor = 1.0
+        for parameters in itemsParameters {
+
+            let width = self.frame.size.width
+            let levelIndicator = PGLevelIndicatorView(frame: CGRect(x: 0, y: 0, width: width, height: width), parameters: parameters, radius: CGFloat(factor))
+            levelIndicators.append(levelIndicator)
+            self.addSubview(levelIndicator)
+            levelIndicator.translatesAutoresizingMaskIntoConstraints = false
+            setConstraints(view: levelIndicator, factor: factor)
+            factor -= factorIncrement
+        }
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setConstraints(view: UIView, factor: Double) {
+
+        NSLayoutConstraint.init(item: view, attribute: .centerX, relatedBy: .equal, toItem: self, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint.init(item: view, attribute: .centerY, relatedBy: .equal, toItem: self, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint.init(item: view, attribute: .width, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 1, constant: 0).isActive = true
+        NSLayoutConstraint.init(item: view, attribute: .height, relatedBy: .equal, toItem: self, attribute: .width, multiplier: 1, constant: 0).isActive = true
+
+    }
+    public func setLevelValue(index: Int, value: Double) {
+        guard index < levelIndicators.count else { return }
+        levelIndicators[index].setValue(value: value)
+    }
+
+}
