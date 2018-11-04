232785a8c9b7d512c6fd6310c0c0ff3dead4fc21
Pablo Garcia <pablo@pablogs.io>
2018-11-04 11:19:56 +0100

    swiftlint

--- PGLevelIndicator/PGLevelIndicator/ViewController.swift
+++ PGLevelIndicator/PGLevelIndicator/ViewController.swift
@@ -17,16 +17,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let parameters = [ItemParameters(title: "nO2 Band", titleColor: UIColor.red, level: .low), ItemParameters(title: "o3 Band", titleColor: UIColor.yellow, level: .medium), ItemParameters(title: "pM10 Band", titleColor: UIColor.green, level: .high), ItemParameters(title: "pM25 Band", titleColor: UIColor.orange, level: .low)]
+        let parameters = [ItemParameters(title: "nO2 Band", titleColor: UIColor.purple, level: .low), ItemParameters(title: "o3 Band", titleColor: UIColor.yellow, level: .medium), ItemParameters(title: "pM10 Band", titleColor: UIColor.green, level: .high), ItemParameters(title: "pM25 Band", titleColor: UIColor.orange, level: .low)]
         levelIndicator = PGLevelIndicator(frame: CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: self.view.frame.size.width), itemsParameters: parameters)
         self.view.addSubview(levelIndicator!)
 
         self.view.backgroundColor = UIColor.black
-//        NSLayoutConstraint.init(item: levelIndicator, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
-//        NSLayoutConstraint.init(item: levelIndicator, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 1, constant: 0).isActive = true
-//        NSLayoutConstraint.init(item: levelIndicator, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1, constant: 0).isActive = true
-//        NSLayoutConstraint.init(item: levelIndicator, attribute: .height, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1, constant: 0).isActive = true
-
     }
     @IBAction func changeValue(_ sender: Any) {
 
