4c3b4723285beb458cdf342a67875c6be35f3988
Pablo Garcia <pablo@pablogs.io>
2019-01-22 19:51:20 +0100

    README Update

--- PGLevelIndicator/PGLevelIndicator/ViewController.swift
+++ PGLevelIndicator/PGLevelIndicator/ViewController.swift
@@ -28,22 +28,35 @@ class ViewController: UIViewController {
         let font: UIFont? = UIFont(name: "Avenir-Book", size: 18)
         let fontSuper: UIFont? = UIFont(name: "Avenir-Book", size: 10)
 
-        let attStringNO2: NSMutableAttributedString = NSMutableAttributedString(string: "NO2", attributes: [.font: font!])
-        attStringNO2.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 2, length: 1))
+        let attStringNO2 = NSMutableAttributedString(string: "NO2", attributes: [.font: font!])
 
-        let attStringO3: NSMutableAttributedString = NSMutableAttributedString(string: "O3", attributes: [.font: font!])
-        attStringO3.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 1, length: 1))
+        attStringNO2.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                                   range: NSRange(location: 2, length: 1))
 
-        let attStringpM10: NSMutableAttributedString = NSMutableAttributedString(string: "PM10", attributes: [.font: font!])
-        attStringpM10.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 2, length: 2))
+        let attStringO3 = NSMutableAttributedString(string: "O3", attributes: [.font: font!])
 
-        let attStringPM25: NSMutableAttributedString = NSMutableAttributedString(string: "PM2.5", attributes: [.font: font!])
-        attStringPM25.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 2, length: 3))
+        attStringO3.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                                  range: NSRange(location: 1, length: 1))
 
-        let attStringSO2: NSMutableAttributedString = NSMutableAttributedString(string: "SO2", attributes: [.font: font!])
-        attStringSO2.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 2, length: 1))
+        let attStringpM10 = NSMutableAttributedString(string: "PM10", attributes: [.font: font!])
 
-        let parameters = [ItemParameters(title: attStringNO2, titleColor: UIColor.red, level: .low), ItemParameters(title: attStringO3, titleColor: UIColor.yellow, level: .medium), ItemParameters(title: attStringpM10, titleColor: UIColor.green, level: .high), ItemParameters(title: attStringPM25, titleColor: UIColor.orange, level: .low)]
+        attStringpM10.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                                    range: NSRange(location: 2, length: 2))
+
+        let attStringPM25 = NSMutableAttributedString(string: "PM2.5", attributes: [.font: font!])
+
+        attStringPM25.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                                    range: NSRange(location: 2, length: 3))
+
+        let attStringSO2 = NSMutableAttributedString(string: "SO2", attributes: [.font: font!])
+
+        attStringSO2.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                                   range: NSRange(location: 2, length: 1))
+
+        let parameters = [ItemParameters(title: attStringNO2, titleColor: UIColor.red, level: .low),
+                          ItemParameters(title: attStringO3, titleColor: UIColor.yellow, level: .medium),
+                          ItemParameters(title: attStringpM10, titleColor: UIColor.green, level: .high),
+                          ItemParameters(title: attStringPM25, titleColor: UIColor.orange, level: .low)]
         return parameters
     }
     @IBAction func changeValue(_ sender: Any) {
--- README.md
+++ README.md
@@ -31,15 +31,31 @@ Just drag and drop [Source](https://github.com/pablogsIO/PGLevelIndicator/tree/m
 
 - Create attributed strings with the text you want
 
+
 ```swift
-let attStringNO2: NSMutableAttributedString = NSMutableAttributedString(string: "NO2", attributes: [.font: font!])
-       attStringNO2.setAttributes([.font: fontSuper!, .baselineOffset: -5], range: NSRange(location: 2, length: 1))
+
+let font: UIFont? = UIFont(name: "Avenir-Book", size: 18)
+let fontSuper: UIFont? = UIFont(name: "Avenir-Book", size: 10)
+
+```
+
+```swift
+
+let attStringNO2 = NSMutableAttributedString(string: "NO2", attributes: [.font: font!])
+
+attStringNO2.setAttributes([.font: fontSuper!, .baselineOffset: -5],
+                          range: NSRange(location: 2, length: 1))
+
 ```
 
 - Create an array of parameters:
 
 ```swift
-let parameters = [ItemParameters(title: attStringNO2, titleColor: UIColor.red, level: .low), ItemParameters(title: attStringO3, titleColor: UIColor.yellow, level: .medium), ItemParameters(title: attStringpM10, titleColor: UIColor.green, level: .high), ItemParameters(title: attStringPM25, titleColor: UIColor.orange, level: .low)]
+
+let parameters = [ItemParameters(title: attStringNO2, titleColor: UIColor.red, level: .low),
+                  ItemParameters(title: attStringO3, titleColor: UIColor.yellow, level: .medium),
+                  ItemParameters(title: attStringpM10, titleColor: UIColor.green, level: .high),
+                  ItemParameters(title: attStringPM25, titleColor: UIColor.orange, level: .low)]
 
 ```
 
@@ -48,14 +64,21 @@ let parameters = [ItemParameters(title: attStringNO2, titleColor: UIColor.red, l
 - Create the PGLevelIndicator object:
 
 ```swift
-let levelIndicator = PGLevelIndicator(frame: CGRect(x: 0, y: 100, width: self.view.frame.size.width, height: self.view.frame.size.width), itemsParameters: parameters)
+
+let levelIndicator = PGLevelIndicator(frame: CGRect(x: 0,
+                                                    y: 100,
+                                                    width: self.view.frame.size.width,
+                                                    height: self.view.frame.size.width),
+                                      itemsParameters: parameters)
 
 ```
 
 - And finally, add the levelIndicator to the main view
 
 ```swift
+
 self.view.addSubview(levelIndicator!)
+
 ```
 
 ## Support/Issues
